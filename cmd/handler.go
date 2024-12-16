d920c77511eb622d429078a6b25834ec88db3735
Onolax <meenamonu2002@gmail.com>
2024-12-16 17:43:57 +0530

    File copy system completed

--- cmd/handler.go
+++ cmd/handler.go
@@ -11,6 +11,59 @@ func init() {
 	rootCmd.AddCommand(add)
 	rootCmd.AddCommand(open)
 	rootCmd.AddCommand(write)
+	rootCmd.AddCommand(del)
+	rootCmd.AddCommand(copy)
+
+	copy.Flags().BoolP("append", "a", false, "appends into the file instead of truncating the file")
+}
+
+var copy = &cobra.Command{
+	Use:   "copy",
+	Short: "Copy files",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 2 {
+			fmt.Println("Not enough arguments")
+			return
+		}
+		file, err := os.Open(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		content, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		appen, _ := cmd.Flags().GetBool("append")
+		if appen {
+			file2, err := os.OpenFile(args[1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file2.Close()
+			_, err = file2.WriteString(string(content))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		} else {
+			file2, err := os.Create(args[1])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file2.Close()
+			_, err = file2.WriteString(string(content))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		fmt.Println("Copied the file", args[0], "successfully into", args[1])
+	},
 }
 
 var write = &cobra.Command{
@@ -36,6 +89,27 @@ var write = &cobra.Command{
 		}
 		fmt.Println("writen to file successfully")
 
+	},
+}
+
+var del = &cobra.Command{
+	Use:   "delete",
+	Short: "delete specified file",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("filename is required")
+			return
+		}
+		_, err := os.Stat(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = os.Remove(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	},
 }
 
